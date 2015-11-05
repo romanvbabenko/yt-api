aff87f28737a38628a729cd8f50c4d3a835ba1a2
Roman V. Babenko <romanvbabenko@gmail.com>
2015-11-05 18:23:36 +0200

    [^] spec/requests/tubes fixed

--- spec/requests/tubes_spec.rb
+++ spec/requests/tubes_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe '/tubes' do
 
   it 'return proper json' do
-    get '/tubes', url: 'https://www.googleapis.com/youtube/v3/videos?part=id,snippet,statistics&id=KBzmzdnZe14&key=AIzaSyCrYTzqULvWN9b9NInDdQMpf5t5hy9zRSo'
+    get '/tubes', url: 'https://www.youtube.com/watch?v=KBzmzdnZe14'
 
     expect(response.status).to eq 200
   end
